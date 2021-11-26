177edad9dfacd150826dca685a6b4f13fc4ddfa4
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2021-11-27 01:10:45 +0500

    :heavy_check_mark: 15th challenge code refactorized

--- 15thChallenge/15thChallenge.js
+++ 15thChallenge/15thChallenge.js
@@ -1,19 +1,23 @@
 function hey(message) {
   let bobResponse = "";
-  if (message === "How are you?") {
-    bobResponse = "Sure.";
-  } else if (message === "YELL") {
-    bobResponse = "Whoa, chill out!";
-  } else if (message === "YELL???") {
-    bobResponse = "Calm down, I know what I'm doing!";
-  } else if (message === "") {
-    bobResponse = "Fine.";
-  } else if (message === "") {
-    bobResponse = "Fine. Be that way!";
-  } else {
-    bobResponse = "Whatever.";
+  switch (message) {
+    case "How are you?":
+      bobResponse = "Sure.";
+      break;
+    case "YELL":
+      bobResponse = "Whoa, chill out!";
+      break;
+    case "YELL???":
+      bobResponse = "Calm down, I know what I'm doing!";
+      break;
+    case "":
+      bobResponse = "Fine. Be that way!";
+      break;
+    default:
+      bobResponse = "Whatever.";
   }
   return bobResponse;
 }
 
+console.log(hey("YELL"));
 console.log(hey("How are you?"));
