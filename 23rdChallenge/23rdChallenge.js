08058769c88a826b910462778692cbfee6ccc728
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2021-12-04 00:04:08 +0500

    :heavy_check_mark: 23rd challenge solution added.

---
+++ 23rdChallenge/23rdChallenge.js
@@ -0,0 +1,14 @@
+function findEvenIndex(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let leftSum = 0;
+    let rightSum = 0;
+
+    for (let j = 0; j < i; j++) leftSum += arr[j];
+    for (let j = i + 1; j < arr.length; j++) rightSum += arr[j];
+
+    if (leftSum === rightSum) return i;
+  }
+  return -1;
+}
+
+console.log(findEvenIndex([1, 2, 3, 4, 3, 2, 1]));
