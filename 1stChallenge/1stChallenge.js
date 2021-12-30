a74cf5feaaadafc2b259aaf45f981ab37fc96e2f
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2021-12-31 03:19:46 +0500

    :white_check_mark: 1st Challenge solution updated & Added Test cases in README

--- 1stChallenge/1stChallenge.js
+++ 1stChallenge/1stChallenge.js
@@ -1,5 +1,7 @@
 function randomNumberGeneratorInRange(rangeStart, rangeEnd) {
-  return Math.floor(Math.random() * (rangeEnd - rangeStart) + rangeStart);
+  return ~~(Math.random() * (rangeEnd - rangeStart) + rangeStart);
 }
+
+// Instead of Math.floor() we can use double telda ~~ symbol for conversion
 
 console.log(`My random number: ${randomNumberGeneratorInRange(5, 100)}`);
